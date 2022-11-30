ac666b1b1dd57b76b04dfbb2f2d2d175ceb93786
Emanuel Arroyo <ema201217@gmail.com>
2022-11-30 08:04:37 -0300

    iniciando proyecto fravega-app

---
+++ app.js
@@ -0,0 +1,27 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log(path.join(__dirname))
+app.get('/',(req,res) => {
+  res.sendFile(path.join(__dirname))
+})
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
